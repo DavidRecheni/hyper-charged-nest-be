14ec2b1574352403a1c9820718dea5afe810a251
David Recheni <david@lhotse.de>
2022-05-13 13:10:20 +0200

    Working user tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let module: TestingModule;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    module = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [UsersService],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -16,5 +19,8 @@ describe('UsersController', () => {
     expect(controller).toBeDefined();
   });
 
-  // TODO: Test get /
+  it('should return "Hello World!"', () => {
+    const appController = module.get(UsersController);
+    expect(appController.getHello()).toBe('Hello World!');
+  });
 });
